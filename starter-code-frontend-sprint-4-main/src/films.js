d11a1ef5f475db137fb17615c4a2bca09166a0ba
SergioGarciaF <sergiogarciafernandez4@gmail.com>
2023-07-17 10:37:03 +0200

    Nivel-1-Ejercicio-2

--- starter-code-frontend-sprint-4-main/src/films.js
+++ starter-code-frontend-sprint-4-main/src/films.js
@@ -7,8 +7,11 @@ function getAllDirectors(array) {
 
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
- 
+  const moviesFromDirector = array.filter(movies => movies.director === director);
+  console.log('EXERCICE 2 ->', moviesFromDirector);
+  return moviesFromDirector;
 }
+
 
 // Exercise 3: Calculate the average of the films of a given director.
 function moviesAverageOfDirector(array, director) {
