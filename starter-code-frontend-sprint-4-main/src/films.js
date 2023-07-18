6303b5f02e8f793ea1bf2bacefa9ee14e834a08b
SergioGarciaF <sergiogarciafernandez4@gmail.com>
2023-07-18 10:10:33 +0200

    Nivel-1-Ejercicio-6

--- starter-code-frontend-sprint-4-main/src/films.js
+++ starter-code-frontend-sprint-4-main/src/films.js
@@ -1,6 +1,6 @@
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
-  let result =  array.map(movies => movies.director);
+  let result = array.map(movies => movies.director);
   console.log("EXERCICE 1 ->", result);
   return result;
 }
@@ -17,7 +17,7 @@ function getMoviesFromDirector(array, director) {
 function moviesAverageOfDirector(array, director) {
   const moviesFromDirector = array.filter(movies => movies.director === 'Quentin Tarantino');
 
-  if(moviesFromDirector === 0){
+  if (moviesFromDirector === 0) {
     return 0;
   }
 
@@ -29,27 +29,65 @@ function moviesAverageOfDirector(array, director) {
 
 // Exercise 4:  Alphabetic order by title 
 function orderAlphabetically(array) {
-  
+  let peliculasOrden = [...array].sort(function (a, b) {
+    if (a.title > b.title) {
+      return 1;
+    }
+    if (a.title < b.title) {
+      return -1;
+    }
+    return 0;
+  });
+
+  let max20 = 20;
+  let peliculas = peliculasOrden.slice(0, max20);
+  console.log('EXERCICE 4 ->', peliculasOrden);
+  return peliculas.map(movies => movies.title);
 }
 
 // Exercise 5: Order by year, ascending
-function orderByYear() {
-
+function orderByYear(array) {
+  let yearOrdenado = [...array].sort(function (a, b) {
+    if (a.year > b.year) {
+      return 1;
+    }
+    if (a.year < b.year) {
+      return -1;
+    }
+    if (a.title > b.title) {
+      return 1;
+    }
+    if (a.title < b.title) {
+      return -1;
+    }
+    return 0;
+  })
+  return yearOrdenado;
 }
 
 // Exercise 6: Calculate the average of the movies in a category
-function moviesAverageByCategory() {
+function moviesAverageByCategory(movies, genre) {
+  const arrayFiltro = movies.filter((movies) => movies.genre.includes(genre));
+  const scoreGenero = arrayFiltro.filter((movie) => typeof movie.score === 'number');
 
+  if (scoreGenero.length === 0) {
+    return 0;
+  }
+
+  const resultado = scoreGenero.reduce((sum, genre) => sum + genre.score, 0);
+  const puntuacionTotal = resultado / scoreGenero.length;
+  console.log('EXERCICE 3 ->', puntuacionTotal)
+  return Number(puntuacionTotal.toFixed(2));
 }
 
 // Exercise 7: Modify the duration of movies to minutes
-function hoursToMinutes() {
+function hoursToMinutes(movies) {
 
 }
 
 // Exercise 8: Get the best film of a year
 function bestFilmOfYear() {
-  
+
 }
 
 
